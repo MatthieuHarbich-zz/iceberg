5e0c7335a9b2057523ac7271a1b5d1e6d99180e4
Matthieu Harbich <matthieu.harbich@gmail.com>
2015-03-05 16:09:11 +0100

    hello les gaaars

--- js/main.js
+++ js/main.js
@@ -2,4 +2,25 @@ $(function(){
 	var scene = document.getElementById('scene');
 	var parallax = new Parallax(scene);
 	
+
+	var width = $(window).width();
+	var lefti = width/2 - 290;
+	$("#iceberg img").css({ left: lefti});
+
+	
+
+	$(window).on("resize", function(){
+		var w = $(window).width();
+
+		console.log(width);
+
+		var left = w/2 - 290;
+		console.log(left);
+
+		// $("#iceberg img").parent().css();
+		$("#iceberg img").css({ left: left});
+	})
+
+
+
 });
