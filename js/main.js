320cd7c996ec7591b775d75938b9eb2d73ec207a
Matthieu Harbich <matthieu.harbich@gmail.com>
2015-03-08 00:27:08 +0100

    ready to merge

--- js/main.js
+++ js/main.js
@@ -6,19 +6,19 @@ $(function(){
 	var width = $(window).width();
 	var lefti = width/2 - 290;
 	$("#iceberg img").css({ left: lefti});
+	$("#holder").css({left:lefti, top:203+"px"})
+
 
 	
 
 	$(window).on("resize", function(){
 		var w = $(window).width();
 
-		console.log(width);
-
 		var left = w/2 - 290;
-		console.log(left);
-
+		
 		// $("#iceberg img").parent().css();
 		$("#iceberg img").css({ left: left});
+		$("#holder").css({left:left})
 	})
 
 
